9eb5895405cb2b084273e9606a204c395d81b4c1
Максим Мухомедьяров <madmaxim22@yandex.ru>
2025-08-20 19:24:36 +0300

    impl: set homework

---
+++ src/js/Team.js
@@ -0,0 +1,23 @@
+export class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if(this.members.has(character)) {
+      throw new Error('Этот персонаж уже в команде.');
+    }
+
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    for (const character of characters) {
+      this.members.add(character);
+    }
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+}
---
+++ src/js/__tests__/team.test.js
@@ -0,0 +1,50 @@
+import { Team } from "../Team.js";
+
+describe('Team', () => {
+  class Character {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  let team;
+  let hero1;
+  let hero2;
+  let hero3;
+
+  beforeEach(() => {
+    team = new Team();
+    hero1 = new Character('Hero1');
+    hero2 = new Character('Hero2');
+    hero3 = new Character('Hero3');
+  });
+
+  test('если добавить нового персонажа в команду', () => {
+    team.add(hero1);
+    expect(team.toArray()).toContain(hero1);
+  });
+
+  test('если добавить в команду одного и того же персонажа, получим ошибку', () => {
+    team.add(hero1);
+    expect(() => team.add(hero1)).toThrow('Этот персонаж уже в команде');
+  });
+
+  test('если добавить несколько новых персонажей в команду', () => {
+    team.addAll(hero1, hero2);
+    expect(team.toArray()).toEqual(expect.arrayContaining([hero1, hero2]));
+  });
+
+  test('если добавить в команду несолько одинаковых персонажей', () => {
+    team.addAll(hero1, hero2);
+    team.addAll(hero2, hero3);
+    expect(team.toArray()).toEqual(expect.arrayContaining([hero1, hero2, hero3]));
+  });
+
+  test('если конвертируем Set в массив', () => {
+    team.add(hero1);
+    team.add(hero2);
+    const array = team.toArray();
+    expect(Array.isArray(array)).toBe(true);
+    expect(array).toEqual([hero1, hero2]);
+  });
+});
